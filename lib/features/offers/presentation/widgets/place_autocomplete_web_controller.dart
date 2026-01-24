ec03b6c7300d00de5236c3f2a12fd11b9e5f1fe4
mrr <mohamed-redha@hotmail.fr>
2026-01-24 23:16:30 +0100

    Sync Places UI Kit input with selection

--- lib/features/offers/presentation/widgets/place_autocomplete_web_controller.dart
+++ lib/features/offers/presentation/widgets/place_autocomplete_web_controller.dart
@@ -142,5 +142,24 @@ class PlaceAutocompleteWebController {
     try {
       js_util.callMethod(element, 'setAttribute', ['value', value]);
     } catch (_) {}
+    _setShadowInputValue(element, value);
+  }
+
+  void _setShadowInputValue(Element element, String value) {
+    try {
+      final shadowRoot = js_util.getProperty(element, 'shadowRoot');
+      if (shadowRoot == null) {
+        return;
+      }
+      final input = js_util.callMethod(shadowRoot, 'querySelector', ['input']);
+      if (input == null) {
+        return;
+      }
+      js_util.setProperty(input, 'value', value);
+      try {
+        js_util.callMethod(input, 'dispatchEvent', [Event('input')]);
+        js_util.callMethod(input, 'dispatchEvent', [Event('change')]);
+      } catch (_) {}
+    } catch (_) {}
   }
 }
