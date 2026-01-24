e81d75593a13ce425ffe8f682f534ef64e38df5c
mrr <mohamed-redha@hotmail.fr>
2026-01-24 20:33:48 +0100

    Default Firebase config to enabled

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   static const bool firebaseConfigured =
-      bool.fromEnvironment('FIREBASE_CONFIGURED', defaultValue: false);
+      bool.fromEnvironment('FIREBASE_CONFIGURED', defaultValue: true);
 }
