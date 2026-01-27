f962a74232f89aa15fed24168638dd017f813428
mrr <mohamed-redha@hotmail.fr>
2026-01-27 18:49:55 +0100

    Add ValueChanged import for Places UI controller

--- lib/features/offers/presentation/widgets/place_autocomplete_web_controller.dart
+++ lib/features/offers/presentation/widgets/place_autocomplete_web_controller.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:async';
 import 'dart:html';
+import 'package:flutter/foundation.dart';
 // ignore: uri_does_not_exist
 import 'dart:js_util' as js_util;
 import '../../../../core/config/google_maps_config.dart';
