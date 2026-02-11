11ef91144da9948f50f4fc9c5461926ecd2989b0
mrr <mohamed-redha@hotmail.fr>
2026-02-11 10:45:10 +0100

    Speed up web bootstrap

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,16 +1,9 @@
-// Custom Flutter bootstrap to reduce startup latency.
-window.addEventListener("load", function () {
-  var serviceWorkerVersion = "{{flutter_service_worker_version}}";
+{{flutter_js}}
+{{flutter_build_config}}
 
-  _flutter.loader.load({
-    serviceWorkerSettings: {
-      serviceWorkerVersion: serviceWorkerVersion,
-      timeoutMillis: 1000,
-    },
-    onEntrypointLoaded: function (engineInitializer) {
-      engineInitializer.initializeEngine().then(function (appRunner) {
-        appRunner.runApp();
-      });
-    },
-  });
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "{{flutter_service_worker_version}}",
+    timeoutMillis: 1000,
+  },
 });
