ad11dea93497994ca7c8caf7ef306e7f06de27cf
mrr <mohamed-redha@hotmail.fr>
2026-02-15 13:57:57 +0000

    Revert broken Qwik select popover stabilization

--- apps/qwik/src/components/ui/select.tsx
+++ apps/qwik/src/components/ui/select.tsx
@@ -1,4 +1,4 @@
-import { $, component$, type QRL, useSignal, useVisibleTask$ } from '@builder.io/qwik';
+import { component$, type QRL } from '@builder.io/qwik';
 import { Select as QSelect } from '@qwik-ui/headless';
 import { cn } from '../../lib/ui/cn';
 
@@ -33,40 +33,6 @@ export const Select = component$<SelectProps>((props) => {
     value,
   } = props;
 
-  const triggerRef = useSignal<HTMLElement>();
-  const popoverRef = useSignal<HTMLElement>();
-
-  const syncPopoverWidth$ = $(() => {
-    if (!triggerRef.value || !popoverRef.value) {
-      return;
-    }
-    const width = `${Math.round(triggerRef.value.getBoundingClientRect().width)}px`;
-    popoverRef.value.style.setProperty('--ui-select-trigger-width', width);
-  });
-
-  useVisibleTask$(({ cleanup }) => {
-    const trigger = triggerRef.value;
-    if (!trigger) {
-      return;
-    }
-
-    syncPopoverWidth$();
-
-    const observer = new ResizeObserver(() => {
-      syncPopoverWidth$();
-    });
-    observer.observe(trigger);
-
-    window.addEventListener('resize', syncPopoverWidth$);
-    window.addEventListener('orientationchange', syncPopoverWidth$);
-
-    cleanup(() => {
-      observer.disconnect();
-      window.removeEventListener('resize', syncPopoverWidth$);
-      window.removeEventListener('orientationchange', syncPopoverWidth$);
-    });
-  });
-
   return (
     <QSelect.Root
       class="ui-select-root"
@@ -80,25 +46,14 @@ export const Select = component$<SelectProps>((props) => {
         }
       }}
     >
-      <QSelect.Trigger id={id} class={cn('ui-select', className)} ref={triggerRef}>
+      <QSelect.Trigger id={id} class={cn('ui-select', className)}>
         <QSelect.DisplayValue placeholder={placeholder} />
         <span class="material-icons-outlined ui-select-icon" aria-hidden="true">
           expand_more
         </span>
       </QSelect.Trigger>
 
-      <QSelect.Popover
-        class="ui-select-popover"
-        flip={false}
-        floating="bottom-start"
-        gutter={8}
-        ref={popoverRef}
-        onBeforeToggle$={(event) => {
-          if (event.newState === 'open') {
-            syncPopoverWidth$();
-          }
-        }}
-      >
+      <QSelect.Popover class="ui-select-popover" flip={false}>
         {options.map((option) => (
           <QSelect.Item
             key={option.value}
