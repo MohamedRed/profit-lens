e41050ab4a29bcf93f80949a8a528204cd6d6c95
mrr <mohamed-redha@hotmail.fr>
2026-02-15 11:33:01 +0000

    fix(qwik): disable viewport zoom for app-like /next experience

--- apps/qwik/src/components/router-head/router-head.tsx
+++ apps/qwik/src/components/router-head/router-head.tsx
@@ -14,7 +14,10 @@ export const RouterHead = component$(() => {
       <title>{head.title}</title>
 
       <link rel="canonical" href={loc.url.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+      />
       <link rel="icon" type="image/svg+xml" href={`${base}favicon.svg`} />
 
       {head.meta.map((m) => (
