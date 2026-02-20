f3a66d0312e337b096c0a50ef51896bc1af84d75
mrr <mohamed-redha@hotmail.fr>
2026-02-20 16:04:12 +0000

    Parse vehicle editor query on client

--- apps/qwik/src/routes/app/settings/vehicles/edit/index.tsx
+++ apps/qwik/src/routes/app/settings/vehicles/edit/index.tsx
@@ -1,5 +1,9 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
+import {
+  LoadingSkeletonAnnouncer,
+  SettingsFormSkeleton,
+} from '../../../../components/ui/page-loading-skeleton';
 import { isValidBackToHref } from '../../shared/vehicle-editor-href';
 import { VehicleEditor } from '../vehicle-editor';
 
@@ -18,17 +22,33 @@ const readVehicleId = (search: string): string | null => {
 
 export default component$(() => {
   const location = useLocation();
-  const rawSearch = location.url.search;
-  const params = new URLSearchParams(rawSearch);
-  const vehicleId = readVehicleId(rawSearch);
-  const returnToHref = params.get('backTo');
-  const resolvedBackToHref = isValidBackToHref(returnToHref) ? returnToHref : null;
+  const vehicleId = useSignal<string | null>(null);
+  const resolvedBackToHref = useSignal<string | null>(null);
+  const searchReady = useSignal(false);
+
+  useVisibleTask$(({ track }) => {
+    const search = track(() => location.url.search);
+    const params = new URLSearchParams(search);
+    vehicleId.value = readVehicleId(search);
+    const returnToHref = params.get('backTo');
+    resolvedBackToHref.value = isValidBackToHref(returnToHref) ? returnToHref : null;
+    searchReady.value = true;
+  });
+
+  if (!searchReady.value) {
+    return (
+      <div aria-busy="true">
+        <LoadingSkeletonAnnouncer label="Loading..." />
+        <SettingsFormSkeleton fieldCount={3} />
+      </div>
+    );
+  }
 
   return (
     <VehicleEditor
       mode="edit"
-      vehicleId={vehicleId}
-      returnToHref={resolvedBackToHref}
+      vehicleId={vehicleId.value}
+      returnToHref={resolvedBackToHref.value}
     />
   );
 });
