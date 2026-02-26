b8a9e723aa1a16af11edd35cba5c82831583ffb6
mrr <mohamed-redha@hotmail.fr>
2026-02-26 11:51:34 +0000

    Fix mobile PWA screenshot import by removing token file handoff

--- apps/qwik/src/routes/app/offer/components/offer-flow-content.tsx
+++ apps/qwik/src/routes/app/offer/components/offer-flow-content.tsx
@@ -9,7 +9,6 @@ import { Button } from "../../../../components/ui/button";
 import { t, useI18n } from "../../../../lib/i18n/i18n-context";
 import type { VehicleProfile } from "../../../../lib/types/vehicle";
 import type { OfferAnalysisRecord } from "../offer-analysis-result";
-import { stageOfferScreenshotFile } from "../offer-file-transfer-store";
 import { enableCaptureCta, enableManualEntry } from "../offer-feature-flags";
 import { OfferFlowStatus } from "./offer-flow-status";
 import { OfferManualDetailsSection } from "./offer-manual-details-section";
@@ -28,7 +27,7 @@ interface OfferFlowContentProps {
   minProfitabilityEuro: Signal<number>;
   onAnalyzeManual$: QRL<() => Promise<void>>;
   onClearScreenshotPreview$: QRL<() => void>;
-  onImportScreenshotFile$: QRL<(fileToken: string) => Promise<void>>;
+  onImportScreenshotFile$: QRL<(file: File) => Promise<void>>;
   onSaveProfitabilityTarget$: QRL<(value: string) => Promise<void>>;
   onViewDetails$: QRL<() => void | Promise<void>>;
   payout: Signal<string>;
@@ -58,8 +57,7 @@ export const OfferFlowContent = component$<OfferFlowContentProps>((props) => {
   });
 
   const onFileSelected$ = $(async (file: File) => {
-    const token = stageOfferScreenshotFile(file);
-    await props.onImportScreenshotFile$(token);
+    await props.onImportScreenshotFile$(file);
   });
 
   const onFileInputEvent$ = $(async (element: HTMLInputElement) => {
