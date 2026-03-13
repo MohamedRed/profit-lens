8e104c8888ba3d506ac9f81f9585af345d943d39
mrr <mohamed-redha@hotmail.fr>
2026-03-13 13:45:29 +0000

    Remove Android Remote Config dependency

--- apps/android/app/src/main/java/com/profitlens/android/data/OverlayFeatureFlagsRepository.kt
+++ apps/android/app/src/main/java/com/profitlens/android/data/OverlayFeatureFlagsRepository.kt
@@ -1,9 +1,5 @@
 package com.profitlens.android.data
 
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
-import kotlinx.coroutines.tasks.await
-
 data class OverlayFeatureFlags(
   val uberEatsEnabled: Boolean,
   val deliverooEnabled: Boolean,
@@ -14,23 +10,7 @@ class OverlayFeatureFlagsRepository(private val firebaseReady: Boolean) {
     if (!firebaseReady) {
       return defaultFlags()
     }
-    val remoteConfig = FirebaseRemoteConfig.getInstance()
-    remoteConfig.setConfigSettingsAsync(
-      FirebaseRemoteConfigSettings.Builder()
-        .setMinimumFetchIntervalInSeconds(300)
-        .build(),
-    ).await()
-    remoteConfig.setDefaultsAsync(
-      mapOf(
-        KEY_UBER_EATS to true,
-        KEY_DELIVEROO to true,
-      ),
-    ).await()
-    remoteConfig.fetchAndActivate().await()
-    return OverlayFeatureFlags(
-      uberEatsEnabled = remoteConfig.getBoolean(KEY_UBER_EATS),
-      deliverooEnabled = remoteConfig.getBoolean(KEY_DELIVEROO),
-    )
+    return defaultFlags()
   }
 
   fun defaultFlags(): OverlayFeatureFlags {
@@ -38,10 +18,5 @@ class OverlayFeatureFlagsRepository(private val firebaseReady: Boolean) {
       uberEatsEnabled = true,
       deliverooEnabled = true,
     )
-  }
-
-  private companion object {
-    const val KEY_UBER_EATS = "overlay_provider_uber_eats_enabled"
-    const val KEY_DELIVEROO = "overlay_provider_deliveroo_enabled"
   }
 }
